d83a916fbb0d6d7be9c15f26c962d85a67752082
480814177 <480814177@qq.com>
2019-10-20 23:37:25 +0800

    初始化工具组件

---
+++ Example/Pods/Target Support Files/LGUitls/LGUitls-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LGUitls : NSObject
+@end
+@implementation PodsDummy_LGUitls
+@end
---
+++ Example/Pods/Target Support Files/Pods-LGUitls_Example/Pods-LGUitls_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LGUitls_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LGUitls_Example
+@end
